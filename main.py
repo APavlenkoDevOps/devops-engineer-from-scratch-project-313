cfbf92004bc741ac1308d0ec43b712df89234536
APavlenkoDevOps <andrey.pavlenko.job@gmail.com>
2026-09-14 11:30:42 +0400

    add Dockerfile and sentry integration

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+import sentry_sdk
 
+sentry_dsn = os.getenv("SENTRY_DSN")
+if sentry_dsn:
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        traces_sample_rate=1.0,
+    )
 app = FastAPI()
 
 
